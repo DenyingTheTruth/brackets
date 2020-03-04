4927c525cfeb372cdf3fd09c5f85d1c1bf31fd95
DenyingTheTruth <anton.bachikin@gmail.com>
2020-03-04 11:24:52 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false;
+  let stack = [];
+  stack[0] = str[0];
+
+  for (let i = 1; i < str.length; i++) {
+    stack.push(str[i]);
+    if (stack.length > 1) {
+      checkBracketsConfig(stack, bracketsConfig);
+    }
+  }
+  return stack.length ? false : true;
+};
+
+function checkBracketsConfig(stack, config) {
+  for (let i = 0; i < config.length; i++) {
+    if (
+      stack[stack.length - 2] == config[i][0] &&
+      stack[stack.length - 1] == config[i][1]
+    ) {
+      stack.splice(stack.length - 2, 2);
+    }
+  }
+  return stack;
 }
